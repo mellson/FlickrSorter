26970269bf86e245e67fc9ba8c8a170cee5e7c7d
Anders Bech Mellson <anders@bechmellson.com>
2014-01-28 21:43:03 +0100

    removed flickrkeys.scala

--- src/main/scala/FlickrKeys.scala
+++
@@ -1,7 +0,0 @@
-object FlickrKeys {
-  val apikey = "replace with your own apikey"
-  val secret = "replace with your own secret"
-
-  val authtoken = "replace with your own authtoken"
-  val userid = "replace with your own user id"
-}
